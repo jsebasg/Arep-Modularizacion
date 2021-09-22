4ad030a215a2af936c974c78095cc669006889df
jua nsebastina garcia hincapie <juan.garcia-h@mail.escuelaing.edu.co>
2021-09-22 13:56:47 -0500

    mongo local changes

--- src/main/java/co/edu/escuelaing/virtualization/dockerdemo/MongoDB.java
+++ src/main/java/co/edu/escuelaing/virtualization/dockerdemo/MongoDB.java
@@ -13,6 +13,7 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoIterable;
 
 import java.net.UnknownHostException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -26,7 +27,9 @@ public class MongoDB {
     MongoClientURI uri;
     MongoClient mongoClient;
     public MongoDB(){
-        uri = new MongoClientURI("mongodb://sebs:clave@db:27017/?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&authSource=Arep&authMechanism=SCRAM-SHA-1&3t.uriVersion=3");
+        //uri = new MongoClientURI("mongodb://sebs:clave@db:27017/?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&authSource=Arep&authMechanism=SCRAM-SHA-1&3t.uriVersion=3");
+        uri = new MongoClientURI("mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false");
+        
         mongoClient = new MongoClient(uri);
         MongoIterable<String> list = mongoClient.listDatabaseNames();
         for (String name : list) {
@@ -43,9 +46,11 @@ public class MongoDB {
         System.out.println(collection.countDocuments());
         Document document=new Document();
         document.put("mensaje",message);
+        document.put("fecha", LocalDateTime.now()); 
         System.out.println("entro a add1 DB");
         collection.insertOne(document);
         System.out.println("entro a add2 DB");
+        System.out.println(collection.countDocuments());
     }
     public String getLast(){
         System.out.println("entro a last DB");
@@ -55,11 +60,14 @@ public class MongoDB {
         ArrayList<Document> docs = new ArrayList<Document>();
         ArrayList<String> results = new ArrayList<>();
         fit.into(docs);
+        System.out.println(docs.size());
         for (Document doc : docs) {
-            if (doc.get("mensaje")!= null && doc.get("fecha")!=null){
-                results.add(doc.get("mensaje").toString());
+            if (doc.get("mensaje")!= null){
+                results.add("Elemento: " + doc.get("mensaje").toString() + "<br> Fecha de Creacion: "+ doc.get("fecha").toString()+ "<br>");
             }
+            if(results.size() >= 10 )break;
+            
         }
-        return results.toString();
+        return results.toString().replace(",", "<br>").replace("[", "").replace("]", "");
     }
 }
